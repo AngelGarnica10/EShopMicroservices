32fbba0c34e46c1e58874b897bd453d8d47f445a
AngelGarnica10 <aoga_10@hotmail.com>
2024-03-27 18:41:22 -0600

    Create Products and get products

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //internal interface IQuery<out TResponse> : IRequest<TResponse>
+    //{
+    //}
+
+    public interface IQuery<out TResponse> : IRequest<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
